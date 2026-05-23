6da034ff9cba617bdffc873931ec54b2622f5e53
Pietro <pietrofernandesstefanuto@gmail.com>
2026-05-23 03:06:25 -0300

    Update restaurante.py

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,18 +2,18 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.title()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'Restaurante: {self.nome}, Categoria: {self.categoria}, Ativo: {self.ativo}'
+        return f'Restaurante: {self._nome}, Categoria: {self._categoria}, Ativo: {self._ativo}'
     
     def listar_restaurantes():
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
